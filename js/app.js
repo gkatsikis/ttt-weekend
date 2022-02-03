a13b7d307c3575e1bd25d59619bad1fe1e2faee8
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-03 16:50:52 -0500

    centering elements and changing fonts

--- js/app.js
+++ js/app.js
@@ -7,7 +7,19 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
+const board = document.querySelector('.board')
 
+const topLeft = document.querySelector('#sq0')
+const topCenter = document.querySelector('#sq1')
+const topRight = document.querySelector('#sq2')
+const midLeft = document.querySelector('#sq3')
+const midCenter = document.querySelector('#sq4')
+const midRight = document.querySelector('#sq5')
+const bottomLeft = document.querySelector('#sq6')
+const bottomCenter = document.querySelector('#sq7')
+const bottomRight = document.querySelector('#sq8')
+
+const message = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/

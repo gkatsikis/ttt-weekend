95da53f9bea5b968884f079448b49c8302487541
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-04 17:04:16 -0500

    solving possible solutions

--- js/app.js
+++ js/app.js
@@ -6,10 +6,19 @@
 
 let winner, turn
 
+let winningCombos = [
+  [squares[0],squares[1],squares[2]],
+  [squares[3],squares[4],squares[5]],
+  [squares[6],squares[7],squares[8]],
+  [squares[0],squares[3],squares[6]],
+  [squares[1],squares[4],squares[7]],
+  [squares[2],squares[5],squares[8]],
+  [squares[0],squares[4],squares[8]],
+  [squares[2],squares[4],squares[6]]
+]
 
 
-
-
+let squares
 
 /*------------------------ Cached Element References ------------------------*/
 const board = document.querySelector('.board')
@@ -33,52 +42,44 @@ const gameBoard = document.querySelectorAll('.game-board')
 
 // const squares = [topLeft, topCenter, topRight, midLeft, midCenter, midRight, bottomLeft, bottomCenter, bottomRight]
 //or
-// const squares = []
+
 
 /*----------------------------- Event Listeners -----------------------------*/
-//When you click a square marks as 1(x) if turn = true, else 0(o) if turn = false
+//When you click a square marks as 1(x) if turn = true, else -1(o) if turn = false
+// 5) Next, the app should wait for the user to click a square and call a handleClick function
+  // the handleClick function will...
 
-//when you click reset
+	// 5.1) Obtain the index of the square that was clicked by:
+	  // 5.1.1) "Extracting" the index from an id assigned to the element in the HTML 
+		// Hint: Each id seems to correspond with an index in our board array. How could these be used if
+		// we cleaned them up a bit?
 
-//
+	// 5.2) If the board has a value at the index, immediately return because that square is already taken.
+
+	// 5.3) If winner is not null, immediately return because the game is over.
+
+	// 5.4) Update the board array at the index with the value of turn.
+
+	// 5.5) Change the turn by multiplying turn by -1 (this flips a 1 to -1, and vice-versa).
+
 
 /*-------------------------------- Functions --------------------------------*/
-// 3.1) Call an initialize function
 
-	
-	  // 3.2.2) Initialize whose turn it is to 1 (player 'X'). 
-	    // Player 'O' will be represented by -1.
-	  // 3.2.3) Initialize the winner variable to null.
-	    // This represents that there is no winner or tie yet. 
-	    // The winner variable will hold the player value (1 or -1) if there's a winner. 
-	    // The winner will hold a 'T' if there's a tie.
-	  // 3.2.4) Render those state variables to the page by calling a render function.
 init()
 
 function init(evt) {
 //make board array to 9 nulls
 squares = [null, null, null, null, null, null, null, null, null]
 //initialize who's turn
-turn = 1 //player 'X'
+turn = -1 //player 'X'
 //make winner variable = null
 winner = null
 render()
 }
 
-
-	  // 3.3.2) Render a message reflecting the current game state:
-	    // 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
-	      // Hint: Maybe use a ternary inside of a template literal here?
-	    // 3.3.2.2) If winner is equal to 'T' (tie), render a tie message.
-	    // 3.3.2.3) Otherwise, render a congratulatory message to which player has won.
-	      // Hint (again): Maybe use a ternary inside a template literal here
-
-		// 3.4) After completing this step, you should be able to manually change the values held in the board array in the initialization function and see the style of the corresponding square change on your page.
-
 function render(evt) {
-  //loop over the board array
   renderBoard()
-  
+  renderMessage()
 }
 
 function renderBoard(evt) {
@@ -93,27 +94,53 @@ function renderBoard(evt) {
 }
 
 function renderMessage(evt) {
-  
+  winner === null ? renderTurn() : renderWinner()
 }
-// function play(evt) {
-//   if (turn = true) {
-//     X
-//   } else {
-//     O
-//   }
-// }
+
+function renderTurn(evt) {
+  turn *= -1
+  // console.log(turn)
+  turn === 1 ? message.textContent = "It's X's turn" : message.textContent = "It's O's turn"  
+}
+
+function renderWinner(evt) {
+  if (winner === 'T') {
+    message.textContent = "It's a tie!"
+  } else if (winner === 1) {
+    message.textContent = "X's have won it!"
+  } else {
+    message.textContent = "O's have won it!"
+  }
+}
+
+
+
+
+
+
+
+
+// 5.6.1) There are a couple methods you can use to find out if there is a winner.
+	   // This is the first, more elegant way that takes advantage of the winningCombos array you wrote above in step 4.
+	   // The 5.6.2 step is a little simpler to comprehend, but you'll need to write a lot more code.
+	   // The 5.6.2 step also won't take advantage of the winningCombos array, but using it as a reference will help you build a solution.
+	   // Choose only one path.
+		  // 5.6.1.1) Loop through the each of the winning combination arrays defined.
+		  // 5.6.1.2) Total up the three board positions using the three indexes in the current combo.
+		  // 5.6.1.3) Convert the total to an absolute value (convert any negative total to positive).
+		  // 5.6.1.4) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
+
+		// 5.6.2) This solution is less elegant, but might be easier to write on your own if you're struggling with the 5.6.1.X pseudocode.
+		  // 5.6.2.1) For each one of the winning combinations you wrote in step 4, find the total of each winning combination.
+		  // 5.6.2.2) Convert the total to an absolute value (convert any negative total to positive)
+		  // 5.6.2.3) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
+
 
 //resetting the board
 function reset(evt) {
   clearBoard()
   toggleResetButton()
   resetMessage()
-}
-
-
-
-function takeTurns(evt) {
-
 }
 
 
@@ -138,4 +165,5 @@ function toggleResetButton(evt) {
 function resetMessage(evt) {
   message.innerText = "Ready to play again?"
 }
+
 
